e5bdc5f743cf42d40f37f5b6058b9dbf6ed263f7
Uli Plabst <up@mogular.com>
2023-11-22 10:21:57 +0100

    issue repro

---
+++ ngpsql_jsonelement_issue_repro/Migrations/20231122091327_initial.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ngpsql_jsonelement_issue_repro.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Models",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Json = table.Column<JsonElement>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Models", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Models");
+        }
+    }
+}
---
+++ ngpsql_jsonelement_issue_repro/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.VisualBasic;
+using Npgsql;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+
+namespace ngpsql_jsonelement_issue_repro
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var db = new AppDbContext();
+            var pendingMigrations = await db.Database.GetPendingMigrationsAsync();
+            if (pendingMigrations.Any())
+            {
+                await db.Database.MigrateAsync();
+            }
+
+            var models = await db.Models.ToListAsync(); //Exception occurs here
+            Console.WriteLine("Finished");
+        }
+    }
+
+
+    public class Model
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set;  }
+        public JsonElement? Json { get; set; }
+    }
+
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Model> Models { get; set; }
+
+        protected static NpgsqlDataSource _dataSource = null!;
+        protected static NpgsqlDataSource GetDataSource()
+        {
+            if (_dataSource != null)
+                return _dataSource;
+
+
+            var builder = new NpgsqlConnectionStringBuilder("Host=localhost;Port=5432;Database=npgsql_jsonelement_issue_repro;Username=postgres;")
+            {
+                Password = Environment.GetEnvironmentVariable("%PG_PASSWD%")
+            };
+
+            var sourceBuilder = new NpgsqlDataSourceBuilder(builder.ToString())
+                .EnableDynamicJson();
+
+            _dataSource = sourceBuilder.Build();
+            return _dataSource;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder opt)
+        {
+            base.OnConfiguring(opt);
+            opt.UseNpgsql(GetDataSource());
+        }
+    }
+}
